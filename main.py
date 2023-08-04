6bbed12a9a6ebd961505e20089270701e8656bce
Rob T <Robert.Tootill98@gmail.com>
2023-08-04 09:35:40 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,118 @@
+import random
+
+import streamlit as st
+
+from qdrant_client.models import Distance, VectorParams
+from qdrant_client.http.models import PointStruct, Filter, FieldCondition, MatchValue
+
+from qdrant_connection import QDrantConnection
+
+# qdrant connector demo code
+def main():
+  conn = st.experimental_connection('qdrant', type=QDrantConnection,
+    url='https://789ba2f0-ca62-4c31-bc04-e60a07c83e70.us-east-1-0.aws.cloud.qdrant.io',
+    api_key='C3U6coCz6dNCCkHUFrYIJE9Qpvt8Zq8ogIKlX62KLjMdCc8v4RlH6w'
+  )
+  print(conn)
+
+  cursor = conn.cursor()
+  print(cursor)
+
+  # collection vars
+  collection_name = 'myTestCollection'
+  vector_size = 5
+
+  # delete collection
+  conn.delete_collection(collection_name)
+
+  # create a collection
+  conn.create_collection(collection_name,
+    VectorParams(size=vector_size, distance=Distance.COSINE))
+  
+  # add vectors to collection
+  points = qdrant_addVectors(conn, collection_name, vector_size)
+
+  # get collection
+  collection = conn.get_collection(collection_name)
+  print('-----\ncollection:')
+  print(collection)
+
+  # query with filtering
+  qdrant_filter(conn, collection_name, points[0].vector, vector_size)
+
+# demo code for adding vectors
+def qdrant_addVectors(conn, collection_name, vector_size):
+  # generate random list of vectors
+  cities = [
+    'London',
+    'Berlin',
+    'Paris',
+    'Madrid',
+    'Amsterdam',
+    'Brussels',
+    'Warsaw',
+    'Zurich',
+    'Vienna',
+    'Rome',
+    'Prague',
+    'Lisbon'
+  ]
+
+  points = []
+
+  for i in range(25):
+    # build vector
+    vector = []
+
+    for _ in range(vector_size):
+      vector.append(random.random())
+
+    # build payload
+    containsAllCountries = False
+    stop = False
+
+    payload = {'city': []}
+
+    while(not containsAllCountries and not stop):
+      # choose random city to add
+      city = random.choice(cities)
+
+      if(not city in payload['city']):
+        payload['city'].append(city)
+
+      containsAllCountries = len(payload['city']) == len(cities)
+
+      # randomly choose to stop so all cities are not always chosen
+      stop = random.random() < 0.3
+
+    # add point
+    points.append(PointStruct(id=i, vector=vector, payload=payload))
+
+  print('-----\npoints:')
+  print(points)
+
+  conn.add_vectors(collection_name, points)
+
+  return points
+
+# demo of filter capability
+def qdrant_filter(conn, collection_name, vector, vector_size):
+  # build filter
+  filter = Filter(
+    must=[
+      FieldCondition(
+        key='city',
+        match=MatchValue(value='London')
+      )
+    ]
+  )
+
+  print('-----\nfilter:')
+  print(filter)
+
+  filteredCollection = conn.query(collection_name, vector, filter,
+    limit=vector_size)
+  print('-----\nfilteredCollection: (' + str(len(filteredCollection)) + ')')
+  print(filteredCollection)
+
+main()
---
+++ qdrant_connection.py
@@ -0,0 +1,66 @@
+from streamlit.connections import ExperimentalBaseConnection
+from streamlit.runtime.caching import cache_data
+
+import pandas as pd
+
+from qdrant_client import QdrantClient
+
+client = QdrantClient(":memory:")
+
+class QDrantConnection(ExperimentalBaseConnection[QdrantClient]):
+  def _connect(self, **kwargs):
+    qdrant_client = QdrantClient(
+      url=kwargs.pop('url'),
+      api_key=kwargs.pop('api_key')
+    )
+
+    # some error handling...
+
+    # store connected client
+    self.qdrant_client = qdrant_client
+
+  def cursor(self) -> QdrantClient:
+    return self.qdrant_client
+  
+  # crete
+  def create_collection(self, collection_name: str, vectors_config):
+    return self.cursor().recreate_collection(
+      collection_name=collection_name,
+      vectors_config=vectors_config
+    )
+
+  # retrieve
+
+  # get a single collection
+  def get_collection(self, collection_name: str):
+    return self.cursor().get_collection(collection_name=collection_name)
+  
+  # get collection with some filtering
+  def query(self, collection_name: str, query_vector: str, filter, ttl: int=3600, **kwargs):
+    @cache_data(ttl=ttl)
+    def _query(collection_name: str, query_vector: str, _filter, **kwargs):
+      # default limit as 1
+      limit = 1
+
+      if('limit' in kwargs):
+        limit = kwargs.pop('limit')
+      
+      return self.cursor().search(
+        collection_name=collection_name,
+        query_vector=query_vector,
+        query_filter=_filter,
+        limit=limit
+      )
+    
+    return _query(collection_name, query_vector, filter, **kwargs)
+  
+  # update-ish
+
+  # add vectors
+  def add_vectors(self, collection_name: str, points):
+    return self.cursor().upsert(collection_name=collection_name, wait=True,
+      points=points)
+  
+  # delete
+  def delete_collection(self, collection_name: str):
+    return self.cursor().delete_collection(collection_name=collection_name)
